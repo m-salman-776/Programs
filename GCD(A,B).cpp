5daaac57801a81d41c8ad8ab8ca68a4bb0bcc3dc
Salman Ansari <salman.ansari776@gmail.com>
2018-12-26 22:09:07 +0530

    Update and rename GCD(A,B) to GCD(A,B).cpp

--- GCD(A,B).cpp
+++ GCD(A,B).cpp
@@ -24,11 +24,11 @@ Summpose we need to find GCD(A,B)
          return gcd;
         }
 }
-Time Complexity:O(min(A,B))
+/*Time Complexity:O(min(A,B))
 
 Euclid's Method
         This is recursive solution base on modulous appraoch 
-        According to this GCD(A,B) = GCD(B,A%B)
+        According to this GCD(A,B) = GCD(B,A%B)*/
         
         int GCD(int A, int B) 
         {
@@ -39,7 +39,7 @@ Euclid's Method
         }
         
         
-Example
+/*Example
 If a = 16 and B = 10, then the GCD is computed as follows:
 
 GCD(16, 10) = GCD(10, 16 % 10) = GCD(10, 6)
@@ -47,5 +47,5 @@ GCD(10, 6) = GCD(6, 10 % 6) = GCD(6, 4)
 GCD(6, 4) = GCD(4, 6 % 4) = GCD(4, 2)
 GCD(4, 2) = GCD(2, 4 % 2) = GCD(2, 0)
 
-Time Complexity:O(log(MAX(A,B)))
+Time Complexity:O(log(MAX(A,B)))*/
 
